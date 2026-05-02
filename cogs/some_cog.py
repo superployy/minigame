ea3f1e278f8342b55b07860f57f60292f48aa798
superployy <ddaraleak@gmail.com>
2026-05-02 15:24:53 +0700

    Rename SomeCog to General and add commands

--- cogs/some_cog.py
+++ cogs/some_cog.py
@@ -1,9 +1,26 @@
 import discord
 from discord.ext import commands
 
-class SomeCog(commands.Cog):
+class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def ping(self, ctx):
+        """Check the bot's latency"""
+        await ctx.send(f"🏓 Pong! `{round(self.bot.latency * 1000)}ms`")
+
+    @commands.command()
+    async def help(self, ctx):
+        """Show bot commands"""
+        embed = discord.Embed(
+            title="🎮 MOBA GP Mini Games Bot",
+            description="Commands for the bot",
+            color=discord.Color.green()
+        )
+        embed.add_field(name="!ping", value="Check bot latency", inline=False)
+        embed.add_field(name="More games coming soon!", value="Rock Paper Scissors, Tic Tac Toe, Trivia", inline=False)
+        await ctx.send(embed=embed)
+
 async def setup(bot):
-    await bot.add_cog(SomeCog(bot))
+    await bot.add_cog(General(bot))
